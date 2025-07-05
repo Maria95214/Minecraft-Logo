e16e9f37c19e40f2a1944e7145a394599e94e416
Maria95214 <marybgx893@gmail.com>
2025-07-05 12:57:56 +0300

    Add files via upload

---
+++ иконка майнкрвфта.py
@@ -0,0 +1,579 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+t = turtle.Turtle()
+t.speed(10)
+t.pensize(2)
+t.color("#90EE90")
+t.begin_fill()
+t.up()
+t.goto(-130, 100)
+t.pendown()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#006400")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#228B22")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#90EE90")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+    
+for _ in range(2):
+    t.up
+    t.forward(30)
+    t.pendown()
+
+    t.color("#228B22")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.left(90)
+    t.end_fill()
+    
+    t.up
+    t.forward(30)
+    t.pendown()
+    
+    t.color("#006400")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.left(90)
+    t.end_fill()
+
+t.right(90)
+
+t.color("#228B22")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.right(90)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()   
+
+t.up
+t.forward(30)
+
+t.color("#228B22")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#006400")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#1B542C")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#006400")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()  
+
+t.up
+t.forward(30)
+
+t.color("#006400")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.left(90)
+t.forward(30)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.left(90)
+
+t.color("#818479")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+for _ in range(2):
+    
+    t.up
+    t.forward(30)
+    t.pendown()
+
+    t.color("#4C2F26")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.right(90)
+    t.end_fill()
+ 
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#228B22")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+t.pendown()
+
+t.color("#006400")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+
+t.right(90)
+t.forward(30)
+t.pendown()
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.right(90)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#995F4D")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.up
+t.left(90)
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.left(90)
+
+for _ in range(2):
+    t.color("#4C2F26")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.right(90)
+    t.end_fill()
+    t.up
+    t.forward(30)
+    
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+t.color("#995F4D")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.up
+t.forward(30)
+
+for _ in range(2):
+    t.color("#4C2F26")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.right(90)
+    t.end_fill()
+    t.up
+    t.forward(30)
+
+t.right(90)
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.right(90)
+t.forward(30)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#818479")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+for _ in range(2):
+    t.color("#4C2F26")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.left(90)
+    t.end_fill()
+    t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#995F4D")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.left(90)
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.left(90)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#995F4D")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#995F4D")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.right(90)
+t.end_fill()
+
+t.right(90)
+t.forward(30)
+
+t.color("#995F4D")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.right(90)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#885444")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+for _ in range(2):
+    t.color("#663F33")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(30)
+        t.left(90)
+    t.end_fill()
+    t.forward(30)
+
+t.color("#818479")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#663F33")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.forward(30)
+
+t.color("#4C2F26")
+t.begin_fill()
+for _ in range(4):
+    t.forward(30)
+    t.left(90)
+t.end_fill()
+
+t.hideturtle()
+screen.mainloop()
